c96ecb116e61420e856819de7d7330f71ded9302
fossam1 <fossam1212@gmail.com>
2024-09-03 10:10:06 -0500

    Upload assignment programs hello world

---
+++ Hello Part 2.py
@@ -0,0 +1,14 @@
+# Samuel Foss
+# Hello World part 2
+# 9/3/2024
+
+print("\"Do you see a man skillful in his work?\n\nHe will stand before kings;\n\nhe will not stand before obscure men.\"\n\n\t(proverbs 22:29)")
+#output: "Do you see a man skillful in his work?
+
+#He will stand before kings;
+
+#he will not stand before obscure men."
+
+#	(proverbs 22:29)
+
+#Process finished with exit code 0
---
+++ Hello world.py
@@ -0,0 +1,8 @@
+# Samuel Foss
+# Hello World part 1
+# 9/3/2024
+
+print('Hello World')
+#output:Hello World
+
+#Process finished with exit code 0\
